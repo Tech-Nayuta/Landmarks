fff3cafe705aea5c228851a093a8a4157ecb8e8e
Tech-Nayuta <ketanayutawork@gmail.com>
2021-06-28 12:58:16 +0900

    Working with UI Controls

---
+++ Landmarks/Views/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by 化田晃平 on R 3/06/28.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+
+    static let `default` = Profile(username: "g_kumar")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { self.rawValue }
+    }
+}
